711059cb0d8b53018f4355a20f3c21aa30e5271a
bq574lk3r <bq.574lk3r@gmail.com>
2023-03-27 08:44:37 +0300

    DEVELOPMENT. slider.

--- src/js/app.js
+++ src/js/app.js
@@ -6,32 +6,126 @@ flsFunctions.isWebp();
 
 Swiper.use([Navigation]);
 
-const swiper = new Swiper(".first-screen-slider-inner", {
-  slidesPerView: 7,
+const swiperTechnology = new Swiper(".first-screen-slider-inner", {
+
   loop: true,
   navigation: {
     nextEl: ".swiper-arrow-next",
     prevEl: ".swiper-arrow-prev",
   },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints:{
+    567: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 7,
+    }
+  }
 });
 
 const swiperExperience = new Swiper(".fourth-screen-slider-inner", {
-  slidesPerView: 5,
   loop: true,
   navigation: {
     nextEl: ".swiper-arrow-next",
     prevEl: ".swiper-arrow-prev",
   },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints:{
+    567: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    }
+  }
 });
 
 const swiperVacancy = new Swiper(".vacancy-slider", {
-  slidesPerView: 4,
   loop: true,
   navigation: {
     nextEl: ".swiper-vacancy-arrow-next",
     prevEl: ".swiper-vacancy-arrow-prev",
   },
+  breakpoints:{
+    567: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    }
+  }
 });
+
+const swiperPrefooterBitrix = new Swiper(".bitrix-gallery", {
+  loop: true,
+  navigation: false,
+
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints:{
+    567: {
+      slidesPerView: 1,
+    },
+    768: {
+      spaceBetween:5,
+      slidesPerView: 2,
+    },
+    992: {
+      spaceBetween:10,
+      slidesPerView: 3,
+    },
+    1200: {
+      spaceBetween:20,
+      slidesPerView: 4,
+    }
+  }
+});
+
+const casesGallery = new Swiper('.page-cases-gallery',{
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-arrow-next",
+    prevEl: ".swiper-arrow-prev",
+  },
+  breakpoints:{
+    567: {
+      slidesPerView: 1,
+    },
+    768: {
+      spaceBetween:5,
+      slidesPerView: 2,
+    },
+    1200: {
+      spaceBetween:10,
+      slidesPerView: 3,
+    }
+  }
+});
+
 
 $('.header-menu-item-services').click(function () {
   $('.dropdown').fadeToggle(200);
